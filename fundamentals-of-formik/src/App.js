284b0890f161c65c29807d5c4809315b974fd577
Debarun Mitra <debarun96mitra@gmail.com>
2021-12-09 21:18:05 +0530

    basic setup added

---
+++ fundamentals-of-formik/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import logo from './logo.svg';
+import './App.css';
+import MediaForm from './components/MediaForm'
+
+function App() {
+  return (
+    <div className="App">
+      <MediaForm />
+    </div>
+  );
+}
+
+export default App;
