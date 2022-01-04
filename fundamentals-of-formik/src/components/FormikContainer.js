8fb83a01212c5bf0162c8df70a4e17b5128c8800
DebarunMitra <debarun96mitra@gmail.com>
2022-01-04 23:21:01 +0530

    FormikContainer created

---
+++ fundamentals-of-formik/src/components/FormikContainer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import FormikControl from './FormikControl'
+
+const FormikContainer = (props) => {
+  const initialValues = {
+    email: ''
+  }
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid Email Format!').required('Required'),
+  })
+  const onSubmit = values => console.log('Form data', values);
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+      >
+      {
+        formik => (
+          <Form>
+          <FormikControl
+            control='input'
+            type='email'
+            label='Email'
+            name='email'
+          />
+        <button type='submit'>Submit</button>
+      </Form>
+    )}
+    </Formik>
+  )
+}
+
+export default FormikContainer
